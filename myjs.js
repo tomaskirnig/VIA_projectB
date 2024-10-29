34389489110e8c210db4204cadc313237764f9f2
Tomas Kirnig <tomas.kirnig.st@vsb.cz>
2024-10-29 17:41:52 +0100

    Created Album generation func

--- myjs.js
+++ myjs.js
@@ -1,16 +1,58 @@
+titlesAndCovers = 
+[
+  {
+    "title": "Kanye",
+    "cover": "example.jpg"
+  },
+  {
+    "title": "Eminem",
+    "cover": "example.jpg"
+  },
+  {
+    "title": "JavaScript: The Good Parts",
+    "cover": "example.jpg"
+  }
+]
+
+
+function createAlbum(title, cover){
+  var album = document.createElement('div');
+  album.classList.add('album');
+
+  var img = document.createElement('img');
+  img.src = cover;
+
+  var p = document.createElement('p');
+  p.textContent = title;
+
+  album.appendChild(img);
+  album.appendChild(p);
+}
+
+function titlePageAlbums(){
+  var albumGrid = document.querySelector('.album-grid');
+
+  titlePageAlbums.forEach(element => {
+    createAlbum(element.title, element.cover);
+    albumGrid.appendChild(album);
+  });
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
-    const playButton = document.querySelector('.player-controls button:nth-child(2)');
+    const playButton = document.querySelector('#playBtn');
     let isPlaying = false;
-  
+    titlePageAlbums();
     playButton.addEventListener('click', function() {
       if (!isPlaying) {
         playButton.textContent = 'Pause';
         isPlaying = true;
-        // Logic to play music can be added here
+        // Code to play music 
       } else {
         playButton.textContent = 'Play';
         isPlaying = false;
-        // Logic to pause music can be added here
+        // Code to pause music 
       }
     });
   });
